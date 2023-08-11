7edfea88d74b971ac5e41139471e862f930ba09b
Rayhan Khan <rayhankhan1997@gmail.com>
2023-08-11 16:21:15 +0100

    RQP-11. Add first Cypress test to check that we can register a new user

--- tests-cypress/cypress/e2e/user/register.cy.ts
+++ tests-cypress/cypress/e2e/user/register.cy.ts
@@ -1,6 +1,38 @@
-describe("register flow", () => {
-    it("go to register page", () => {
-        cy.visit("/");
-        cy.get("[data-cy=register-form]").should("exist");
-    })
-})
+describe('register flow', () => {
+
+  it('can register a new user', () => {
+    const backendUrl = Cypress.env('BACKEND_URL') || 'http://localhost:8080';
+    const backendRegisterEndpoint = `${backendUrl}/register`;
+
+    cy.intercept('POST', backendRegisterEndpoint, (req) => {
+      req.on('response', async (res) => {
+        res.setDelay(300);
+      });
+    }).as('delayedResponse');
+
+    cy.visit('/register');
+    cy.dataCy('register-form').should('exist');
+
+    const username = `user-${randomString(5)}`;
+
+    cy.dataCy('username-input').type(username);
+    cy.dataCy('email-input').type(`${username}@email.com`);
+    cy.dataCy('password-input').type('password');
+    cy.dataCy('confirmPassword-input').type('password');
+
+    cy.dataCy('register-submit-button').click();
+
+    cy.dataCy('loading-spinner').should('exist');
+
+    cy.wait('@delayedResponse');
+    cy.url().should('contain', '/login');
+  });
+
+  /**
+   * Return a random string with characters from the set [a-zA-Z0-9]
+   * @example const username = `user-${randomString(5)}`
+   */
+  const randomString = (length: number) => {
+    return (Math.random().toString(36) + '00000000000000000').slice(2, length + 2);
+  };
+});
