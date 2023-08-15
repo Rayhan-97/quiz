0d2ce0ded6fd8c0ed38ba684ebaa30dbd7d26d82
Rayhan Khan <rayhankhan1997@gmail.com>
2023-08-15 10:11:35 +0100

    RQP-12. Remove react-native frontend implementations

--- frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // ... other Jest config options ...
-  preset: "jest-expo",
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
-    "\\.(ts|tsx)$": "ts-jest",
-  },
-};
--- quiz-frontend/util/stringUtils.ts
+++
@@ -1,4 +0,0 @@
-const capitalize = (s: string): string =>
-  (s && s[0].toUpperCase() + s.slice(1)) || "";
-
-export default capitalize;
