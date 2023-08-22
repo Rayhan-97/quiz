90266eed6c4577ba2d49b41227ec8cc18fba136a
Rayhan Khan <rayhankhan1997@gmail.com>
2023-08-22 19:38:52 +0100

    RQP-14. Add gradle dependencies

--- backend/core-domain/build.gradle.kts
+++ backend/core-domain/build.gradle.kts
@@ -2,4 +2,8 @@ dependencies {
 	implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
 	implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.2")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("io.jsonwebtoken:jjwt:0.9.1")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("javax.xml.bind:jaxb-api:2.3.1")
 }
--- backend/interface/build.gradle.kts
+++ backend/interface/build.gradle.kts
@@ -2,4 +2,5 @@ dependencies {
 	implementation(project(":backend:core-domain"))
 
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 }
