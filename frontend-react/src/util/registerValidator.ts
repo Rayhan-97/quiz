50af014d7f28122262f3ea2fc302198d8fd47202
Rayhan Khan <rayhankhan1997@gmail.com>
2024-08-21 00:40:48 +0100

    RQP-22. Fix ESLint violations in utilities

--- frontend-react/src/util/registerValidator.ts
+++ frontend-react/src/util/registerValidator.ts
@@ -2,15 +2,15 @@ const USERNAME_REGEX = /^[_A-Za-z0-9-]{6,32}$/;
 export const EMAIL_REGEX =
   /^[_A-Za-z0-9-+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$/;
 
-export const isValidUsername = (username: string = ""): boolean => {
+export const isValidUsername = (username: string = ''): boolean => {
   return USERNAME_REGEX.test(username);
 };
 
-export const isValidEmail = (email: string = ""): boolean => {
+export const isValidEmail = (email: string = ''): boolean => {
   return EMAIL_REGEX.test(email);
 };
 
-export const isValidPassword = (password: string = ""): boolean => {
+export const isValidPassword = (password: string = ''): boolean => {
   return password !== null && password.length >= 8 && password.length < 128;
 };
 
--- frontend-react/src/util/stringUtils.ts
+++ frontend-react/src/util/stringUtils.ts
@@ -1,4 +1,4 @@
 const capitalize = (s: string): string =>
-  (s && s[0].toUpperCase() + s.slice(1)) || "";
+  (s && s[0].toUpperCase() + s.slice(1)) || '';
 
 export default capitalize;
